12dac089a4ed9d311594bc64e1390f37f07c4a3f
nandkishoreesic2017-gif <nandkishoreesic2017@gmail.com>
2026-05-27 15:45:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,23 +165,17 @@ async def TcPChaT(
 
             print("⚠️ Chat socket disconnected")
 
-            try:
-                whisper_writer.close()
-                await whisper_writer.wait_closed()
-            except:
-                pass
+try:
+    whisper_writer.close()
+    await whisper_writer.wait_closed()
 
-            whisper_writer = None
+except Exception as e:
+    print(f"❌ Chat socket error => {e}")
+    traceback.print_exc()
 
-        except Exception as e:
-
-            print(f"❌ Chat socket error => {e}")
-
-            traceback.print_exc()
-
-            whisper_writer = None
-
-        await asyncio.sleep(reconnect_delay)
+whisper_writer = None
+await asyncio.sleep(reconnect_delay)
+           
 
 # ========================= EMOTE SYSTEM =========================
 
